0030c7d9b168dec25f6afe29a52091f2e57f2022
abhishek ranjan <imabhishekranjann1001@gmail.com>
2023-06-29 16:23:26 +0530

    update 2.0

--- app/page.js
+++ app/page.js
@@ -12,14 +12,13 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-
+         
           </Link>
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] text-white  text-5xl">
-        Abhishek Ranjan's project
+        Abhishek Ranjans project
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -31,9 +30,7 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold text-white`}>
             Register part{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-white`}>
             Find in-depth information about Next.js features and API.
@@ -48,12 +45,10 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold text-white`}>
             Dashboard
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-white`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Learn about Next.js in an interactive course with quizzes!
           </p>
         </Link>
 
